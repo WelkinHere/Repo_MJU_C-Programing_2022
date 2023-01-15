59e0c95805648279b5ffadf6cbf9eebd6da8273e
WelkinHere <64088501+WelkinHere@users.noreply.github.com>
2023-01-15 17:06:35 +0800

    修改了注释

--- test_getFileSize.c
+++ test_getFileSize.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    long int fileSize;      // 文件长度，长整型值
-    char fileName[33];      // 文件名，长度不超过32个字符
+    long int fileSize;          // 文件长度，长整型值
+    char fileName[33];          // 文件名，长度不超过32个字符
 
-    scanf("%s", fileName);
+    scanf("%s", fileName);      // 读入文件名
 
-    // 初始化文件指针
-    FILE *fp;
-    fp = fopen(fileName, "w+");
+    FILE *fp;                   // 初始化文件指针
+    fp = fopen(fileName, "w+"); // 打开文件
 
-    fseek(fp, 0, SEEK_END);
+    fseek(fp, 0, SEEK_END);     // 移动文件指针到文件末尾
 
-    fileSize = ftell(fp);
+    fileSize = ftell(fp);       // 将文件末尾处的指针位置输出到变量
 
-    printf("%d", fileSize);
+    printf("%d", fileSize);     // 输出指针位置
 }
