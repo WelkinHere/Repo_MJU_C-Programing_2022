c45404e9812e247e875687a792961d2c10256d68
WelkinHere <64088501+WelkinHere@users.noreply.github.com>
2023-03-05 18:01:00 +0800

    调整文件名

--- 2022学年下学期C语言案例实验/2023-02-27/original - 反弹球消砖块.cpp
+++ 2022学年下学期C语言案例实验/2023-02-27/original - 反弹球消砖块.cpp
@@ -4,16 +4,17 @@
 #include <windows.h>
 
 // 全局变量
-int high,width; // 游戏画面大小
-int ball_x,ball_y; // 小球的坐标
-int ball_vx,ball_vy; // 小球的速度
-int position_x,position_y; // 挡板中心坐标
-int radius; //挡板的半径大小
-int left,right; //挡板左右边界
-int block_x,block_y; //砖块位置
 
-void gotoxy(int x,int y) //光标移动到(x,y)位置
-{
+int high, width; 				// 游戏画面大小
+int ball_x, ball_y; 				// 小球的坐标
+int ball_vx, ball_vy; 			// 小球的速度
+int position_x, position_y; 		// 挡板中心坐标
+int radius; 					// 挡板的半径大小
+int left, right; 				// 挡板左右边界
+int block_x, block_y; 			// 砖块位置
+
+// 光标移动到(x,y)位置
+void gotoXY(int x,int y) {
     HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
     COORD pos;
     pos.X = x;
@@ -21,53 +22,53 @@ void gotoxy(int x,int y) //光标移动到(x,y)位置
     SetConsoleCursorPosition(handle,pos);
 }
 
-void HideCursor() // 用于隐藏光标
-{
+// 用于隐藏光标
+void HideCursor() {
 	CONSOLE_CURSOR_INFO cursor_info = {1, 0};  // 第二个值为0表示隐藏光标
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
 }
 
-void startup()  // 数据初始化
-{
+// 数据初始化
+void startup() {
 	HideCursor();
 	
-	high = 13;   //画布大小初始化
+	high = 13;   				// 画布大小初始化
 	width = 20;
 	
-	ball_x = 0;  //小球位置初始化
+	ball_x = 0;  				// 小球位置初始化
 	ball_y = width/2;
 	
-	ball_vx = 1;  //小球速度初始化
+	ball_vx = 1;  				// 小球速度初始化
 	ball_vy = 1;
 	
-	position_x = high; //挡板位置初始化
+	position_x = high; 			// 挡板位置初始化
 	position_y = width/2;
 	radius = 5;
 	left = position_y - radius;
 	right = position_y + radius;
 	
-	block_x = 0;   //方块位置初始化
+	block_x = 0;   				// 方块位置初始化
 	block_y = 4;
 
 }
 
-void show()  // 显示画面
-{
-	gotoxy(0,0);    // 光标移动到原点位置，以下重画清屏	
+// 显示画面
+void show() {
+	gotoXY(0,0);    			// 光标移动到原点位置，以下重画清屏	
 	int i,j;
 	for (i=0;i<=high+1;i++)
 	{
 		for (j=0;j<=width;j++)
 		{
 			if ((i== ball_x) && (j== ball_y))
-				printf("0");  //   输出小球
+				printf("0");  	// 输出小球
 			else if (j==width)
-				printf("|");  //   输出右边框
+				printf("|");  	// 输出右边框
 			else if (i==high+1)
-				printf("-");  //   输出下边框
+				printf("-");  	// 输出下边框
 			else if ((i==high)&&(j>left)&&(j<right))
 				printf("*");
-			else if ((i==block_x)&&(j==block_y))  //输出砖块
+			else if ((i==block_x)&&(j==block_y))  // 输出砖块
 				printf("C");
 			else
 				printf(" ");  //   输出空格
